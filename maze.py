f7df366052bbdc207853c371f27f1bc40946773f
MarcMitsch <Marcel_Mitschke@gmx.de>
2024-06-27 15:27:18 +0200

    Chapter 6

--- maze.py
+++ maze.py
@@ -65,6 +65,18 @@ class Cell:
         if self.has_bottom_wall:
             self._win.canvas.create_line(self._x1, self._y2, self._x2, self._y2)
 
+    def draw_move(self, to_cell, undo=False):
+        # Calculate the center of the current cell
+        from_x = (self._x1 + self._x2) // 2
+        from_y = (self._y1 + self._y2) // 2
+        # Calculate the center of the target cell
+        to_x = (to_cell._x1 + to_cell._x2) // 2
+        to_y = (to_cell._y1 + to_cell._y2) // 2  
+        # Choose the color based on the undo flag
+        color = "gray" if undo else "red"
+        # Draw the line on the canvas
+        self._win.canvas.create_line(from_x, from_y, to_x, to_y, fill=color, width=2)
+
 
 def main():
     win = Window(800, 600)
