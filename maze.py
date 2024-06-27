543db787f5985716fb26350de6b5cf25308e00c1
MarcMitsch <Marcel_Mitschke@gmx.de>
2024-06-27 14:49:25 +0200

    Chapter 4

--- maze.py
+++ maze.py
@@ -23,8 +23,39 @@ class Window:
     def close(self):
         self.running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+     self.x = x
+     self.y = y
+
+class Line:
+    def __init__(self, start_point, end_point):
+        self.start_point = start_point
+        self.end_point = end_point
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.start_point.x, self.start_point.y,
+            self.end_point.x, self.end_point.y,
+            fill=fill_color, width=2
+)
+
+
 def main():
     win = Window(800, 600)
+    # Create Points
+    point1 = Point(10, 10)
+    point2 = Point(100, 100)
+    point3 = Point(50, 150)
+    # Create Lines
+    line1 = Line(point1, point2)
+    line2 = Line(point3, point1)  
+    # Draw Lines
+    win.draw_line(line1, "black")
+    win.draw_line(line2, "red")
     win.wait_for_close()
 
 
